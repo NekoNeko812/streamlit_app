fc8e880f85b0a78550c2c9ee743ea292fc8e80e2
NekoNeko812 <T125037@ed.sus.ac.jp>
2026-01-27 16:22:36 +0900

    3

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 st.title("都道府県別道路の実延長と舗装済延長の推移")
 st.write("test")
-# df = pd.read_csv("都道府県別道路の実延長と舗装済延長.csv", encoding="shift_jis")
+df = pd.read_csv("都道府県別道路の実延長と舗装済延長.csv", encoding="shift_jis")
 
-# with st.sidebar:
-#   st.subheader("絞り込み条件")
-#   selected_cats = st.multiselect("都道府県を選択してください（複数選択可）",
-#                           df["都道府県"].unique())
+with st.sidebar:
+  st.subheader("絞り込み条件")
+  selected_cats = st.multiselect("都道府県を選択してください（複数選択可）",
+                          df["都道府県"].unique())
 
