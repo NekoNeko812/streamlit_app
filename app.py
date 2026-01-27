c3c2eeba5ba144ec38bb4bebb312dbb7fae4cc70
NekoNeko812 <T125037@ed.sus.ac.jp>
2026-01-27 17:23:13 +0900

    4

--- app.py
+++ app.py
@@ -6,7 +6,30 @@ st.write("test")
 df = pd.read_csv("都道府県別道路の実延長と舗装済延長.csv", encoding="shift_jis")
 
 with st.sidebar:
-  st.subheader("絞り込み条件")
-  selected_cats = st.multiselect("都道府県を選択してください（複数選択可）",
+  st.subheader("表示する都道府県")
+  prefectures = st.multiselect("検索したい都道府県を選択してください（複数選択可）",
                           df["都道府県"].unique())
 
+tab1, tab2, tab3 = st.tabs(["表", "指定年", "複数年"])
+
+with tab1:
+    st.header("表")
+
+    with st.expander("得たい情報選択"):
+          year = st.multiselect("年を指定してください",
+                          list(range(2016, 2025)))
+          want = st.multiselect("得たい情報を選択してください",
+                                ["実延長", "舗装済延長", "舗装率"])
+
+    st.write(year)
+    df = df[df["都道府県"].isin(prefectures)]
+    # df = df[df["media"]==want]
+
+    st.write("単位：km")
+    st.dataframe(df, width=800, height=220)
+
+with tab2:
+    st.write("指定年")
+
+with tab3:
+    st.write("複数年")
