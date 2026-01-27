9b454b12cc14a3880c8095999afc2bbb30b052b3
NekoNeko812 <T125037@ed.sus.ac.jp>
2026-01-27 19:16:23 +0900

    dataframe1.2

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 st.title("都道府県別道路の実延長と舗装済延長の推移")
 st.write("test")
-df = pd.read_csv("都道府県別道路の実延長と舗装済延長.csv", encoding="shift_jis")
 
+df = pd.read_csv("都道府県別道路の実延長と舗装済延長.csv", encoding="shift_jis")
 with st.sidebar:
   st.subheader("表示する都道府県")
   prefectures = st.multiselect("検索したい都道府県を選択してください（複数選択可）",
                           df["都道府県"].unique())
+
+filtered_df = df[df["都道府県"].isin(prefectures)]
 
 tab1, tab2, tab3 = st.tabs(["表", "指定年", "複数年"])
 
@@ -23,13 +25,20 @@ with tab1:
 
     st.write(year)
     st.write(want)
-    df = df[df["都道府県"].isin(prefectures)]
+
+    selected_colmns = ["都道府県"]
     for i in year:
         for j in want:
-            df = df[f"{i}/{j}"]
+            col_name = f"{i}/{j}"
+            if col_name in df.columns:
+                selected_colmns.append(col_name)
 
-    st.write("単位：km")
-    st.dataframe(df, width=800, height=220)
+    if len(year) > 0 and len(want) > 0:
+        display_df = filtered_df[selected_colmns]
+        st.write("単位：km")
+        st.dataframe(display_df)
+    else:
+        st.info("年と得たい情報を選択してください")
 
 with tab2:
     st.write("指定年")
