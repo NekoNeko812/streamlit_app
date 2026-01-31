6a5f5b0d9e728946158d231cd21f0cbf3215d02e
NekoNeko812 <T125037@ed.sus.ac.jp>
2026-02-01 01:00:51 +0900

    wide

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+st.set_page_config(layout="wide")       # 横幅をブラウザに合わせる
+
 st.title("都道府県別道路の実延長と舗装済延長の推移")
 st.write("test")
 
